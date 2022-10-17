f9f1ce8a6697ec78db9dcb2ca77f9662f40177bc
Altruist2692 <altruist2692@gmail.com>
2022-10-17 20:56:46 +0530

    changes for AR 5.1

--- lib/multiparameter_attributes.rb
+++ lib/multiparameter_attributes.rb
@@ -1,7 +1,7 @@
 module ValidatesDateTime
   module MultiparameterAttributes
     def self.included(base)
-      base.alias_method_chain :execute_callstack_for_multiparameter_attributes, :temporal_error_handling
+      base.alias_method :execute_callstack_for_multiparameter_attributes, :temporal_error_handling
     end
     
     def execute_callstack_for_multiparameter_attributes_with_temporal_error_handling(callstack)
